c1dd710464da5a56a8b2f8cb79d882246a731a86
Michel Pavan Macedo <michelpm@gmail.com>
2013-04-27 14:27:06 -0300

    Komenca commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kurso::Application.config.secret_key_base = 'b5f3534850f95a26bb8b422a14b76604a3fc6905dd1789f69c5552844629ee6cd3833c12b5edbaf7f3a15082e3a8db06c59e2267baf9770850ff7e801d692e86'
