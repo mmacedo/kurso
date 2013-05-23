c1f58ad6e933f85438027c4b575223f279446ead
Michel Pavan Macedo <michelpm@gmail.com>
2013-05-22 21:57:50 -0300

    Aldonas lecionon 11n

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,16 @@ Kurso::Application.routes.draw do
   scope "(:locale)", :locale => Regexp.new(lokaĵarojn_ŝablono) do
     root to: 'cxefa#cxefpagxo'
 
-    get 'leciono1', to: 'lecionoj#leciono01', as: 'leciono01'
-    get 'leciono2', to: 'lecionoj#leciono02', as: 'leciono02'
-    get 'leciono3', to: 'lecionoj#leciono03', as: 'leciono03'
-    get 'leciono4', to: 'lecionoj#leciono04', as: 'leciono04'
-    get 'leciono5', to: 'lecionoj#leciono05', as: 'leciono05'
-    get 'leciono6', to: 'lecionoj#leciono06', as: 'leciono06'
-    get 'leciono7', to: 'lecionoj#leciono07', as: 'leciono07'
-    get 'leciono8', to: 'lecionoj#leciono08', as: 'leciono08'
-    get 'leciono9', to: 'lecionoj#leciono09', as: 'leciono09'
+    get 'leciono1',  to: 'lecionoj#leciono01', as: 'leciono01'
+    get 'leciono2',  to: 'lecionoj#leciono02', as: 'leciono02'
+    get 'leciono3',  to: 'lecionoj#leciono03', as: 'leciono03'
+    get 'leciono4',  to: 'lecionoj#leciono04', as: 'leciono04'
+    get 'leciono5',  to: 'lecionoj#leciono05', as: 'leciono05'
+    get 'leciono6',  to: 'lecionoj#leciono06', as: 'leciono06'
+    get 'leciono7',  to: 'lecionoj#leciono07', as: 'leciono07'
+    get 'leciono8',  to: 'lecionoj#leciono08', as: 'leciono08'
+    get 'leciono9',  to: 'lecionoj#leciono09', as: 'leciono09'
     get 'leciono10', to: 'lecionoj#leciono10', as: 'leciono10'
+    get 'leciono11', to: 'lecionoj#leciono11', as: 'leciono11'
   end
 end
